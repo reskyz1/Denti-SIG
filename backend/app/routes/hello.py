d81cc5fd760eb4cbf69cff764cc9c63119bfabb5
Ryan Harmes Eskinazi <141065002+reskyz1@users.noreply.github.com>
2025-06-12 15:23:46 -0300

    fiz bosta

--- backend/app/routes/hello.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-hello_bp = Blueprint('hello', __name__)
-
-@hello_bp.route('/api/hello', methods=['GET'])
-def hello():
-    return jsonify(message="ola do Flask")
