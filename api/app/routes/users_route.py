3f9b59d16a7e85b0fd37824ae8845f0c733a87e1
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-06-14 15:49:47 -0300

    update: run

--- api/app/routes/users_route.py
+++ api/app/routes/users_route.py
@@ -1,10 +1,29 @@
-from flask import Blueprint, jsonify
-from app.services.users_service import register_user
+from flask import Blueprint, request, jsonify
+from api.services.users_service import create_user_dentist, create_user_secretary, create_user_patient
 
-users_bp = Blueprint('user', __name__)  # Cria o blueprint chamado 'user'
+users_bp = Blueprint('users', __name__) #cria um blue prit chamado user 
 
-#api/hello
-@users_bp.route('/login')
-def register():
-    response = register_user()
-    return jsonify(response)
+#Qualquer tipo de usuario. No request deve informar que tipo de usuario é 
+#Responsabilidad do front fazer a verificação da senha
+
+@users_bp.route('/api/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    user_type = data.get('user_type')
+    name = data.get('name')
+    email = data.get('email')
+    password = data.get('password')
+
+    match user_type:
+        case 'dentista':
+            cro = data.get('cro')
+            result, status_code = create_user_dentist(name, email, password, cro)
+        case 'secretary':
+            result, status_code = create_user_secretary(name, email, password)
+        case 'patient':
+            cpf = data.get('cpf') 
+            birth_date = data.get('birth_date')
+            phone = data.get('phone')
+            result, status_code = create_user_patient(name, email, password,cpf, birth_date, phone)
+
+    return jsonify(result), status_code
--- api/app/services/users_service.py
+++ api/app/services/users_service.py
@@ -1,2 +1,7 @@
-def register_user():
-    return "colocar json"
+def create_user_dentist(name, email, password, cro):
+    
+def create_user_secretary(name, email, password): 
+
+def create_user_patient(name, email, password,cpf, birth_date, phone):
+
+    
