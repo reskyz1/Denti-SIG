042ede36d48d6d06b134fb24b9e448a82cd20b85
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-06-14 16:42:01 -0300

    add: user service

--- api/app/services/users_service.py
+++ api/app/services/users_service.py
@@ -1,7 +1,40 @@
+import re
 def create_user_dentist(name, email, password, cro):
     
 def create_user_secretary(name, email, password): 
 
 def create_user_patient(name, email, password,cpf, birth_date, phone):
 
+def login_user_dentist_secretary():
+
+def login_user_patient(email_cpf, password):
+    if validate_cpf(email_cpf):
+        #procurar pelo cpf
+    else:
+        #procurar pelo email
+
+    return {'response': 'sucess login'}, 201
+
+
+# esssa função deveria ta aq?
+def validate_cpf(cpf: str) -> bool:
     
+    if not re.match(r'\d{3}\.\d{3}\.\d{3}-\d{2}', cpf):
+        return False
+
+    numbers = [int(digit) for digit in cpf if digit.isdigit()]
+
+    if len(numbers) != 11 or len(set(numbers)) == 1:
+        return False
+
+    sum_of_products = sum(a*b for a, b in zip(numbers[0:9], range(10, 1, -1)))
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if numbers[9] != expected_digit:
+        return False
+
+    sum_of_products = sum(a*b for a, b in zip(numbers[0:10], range(11, 1, -1)))
+    expected_digit = (sum_of_products * 10 % 11) % 10
+    if numbers[10] != expected_digit:
+        return False
+
+    return True
