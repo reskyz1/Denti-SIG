fc508229998231aa1442f56d5b3b3a0cd88b22b4
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-06-14 16:42:38 -0300

    refactor: run

--- api/run.py
+++ api/run.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from api.app.models.user import db
-from api.app.routes.users_route import users_bp
+import app
+from app.models.user import db
+from app.routes.users_route import users_bp
 
-app = create_app()
+flask_app = app.create_app()
 
-app.register_blueprint(users_bp)
+flask_app.register_blueprint(users_bp)
 
-@app.before_first_request
+@flask_app.before_first_request
 def create_tables():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    flask_app.run(debug=True)
