445921cd6a0c878f84181683751d3852e79f0ab0
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-06-19 16:32:12 -0300

    feat: auth token and toke generetor

---
+++ api/app/utils/token_auth.py
@@ -0,0 +1,62 @@
+from functools import wraps
+import datetime
+from flask import request
+import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def generate_token(user_id):
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    payload = {
+        'sub': user_id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # Token expira em 1 hora
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
+
+#necessario que o header to token siga o padrão 'bearer token'
+def get_token_auth_header():
+    """Obtains the access token from the Authorization Header
+    """
+    auth = request.headers.get("Authorization", None)
+    if not auth:
+        raise AuthError({"code": "authorization_header_missing",
+                        "description":
+                            "Authorization header is expected"}, 401)
+
+    parts = auth.split()
+
+    if parts[0].lower() != "bearer":
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must start with"
+                            " Bearer"}, 401)
+    elif len(parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
+    elif len(parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
+
+    token = parts[1]
+    return token
+
+def requires_auth(f):
+    """Determines if the access token is valid
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = get_token_auth_header()
+        try:
+            SECRET_KEY = os.getenv('SECRET_KEY')
+            payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+            return payload
+        except jwt.ExpiredSignatureError:
+            return None  # Token expirado
+        except jwt.InvalidTokenError:
+            return None  # Token inválido
+    return decorated
