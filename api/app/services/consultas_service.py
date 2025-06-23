15165bd1a793c5abe75a93fadf1f5eb9043232bd
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-06-23 15:15:16 -0300

    feat: validation of appointment time

--- api/app/services/consultas_service.py
+++ api/app/services/consultas_service.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class ConsultaService:
     @staticmethod
     def criar_consulta(data_dict):
-        validar_
+        validar_disponibilidade_consulta(data_dict['data'],data_dict['hora'], data_dict['dentista_id']) 
         nova = Consulta(
             data=datetime.strptime(data_dict['data'], '%Y-%m-%d').date(),
             hora=datetime.strptime(data_dict['hora'], '%H:%M').time(),
@@ -24,10 +24,21 @@ class ConsultaService:
     def atualizar_consulta(id, data_dict):
         consulta = Consulta.query.get_or_404(id)
 
+        # Mudar a data e o horario
+        if 'data' in data_dict and 'hora' in data_dict:
+            if data_dict['data'] != consulta.data and data_dict['hora'] != consulta.hora:
+                validar_disponibilidade_consulta(data_dict['data'], data_dict['hora'], consulta.dentista_id)
+                consulta.data = datetime.strptime(data_dict['data'], '%Y-%m-%d').date()
+                consulta.hora = datetime.strptime(data_dict['hora'], '%H:%M').time()
+        
         if 'data' in data_dict:
-            consulta.data = datetime.strptime(data_dict['data'], '%Y-%m-%d').date()
+            if data_dict['data'] != consulta.data:
+                validar_disponibilidade_consulta(data_dict['data'], consulta.hora, consulta.dentista_id)
+                consulta.data = datetime.strptime(data_dict['data'], '%Y-%m-%d').date()
         if 'hora' in data_dict:
-            consulta.hora = datetime.strptime(data_dict['hora'], '%H:%M').time()
+            if data_dict['hora'] != consulta.hora:
+                validar_disponibilidade_consulta(consulta.data,data_dict['hora'], consulta.dentista_id)
+                consulta.hora = datetime.strptime(data_dict['hora'], '%H:%M').time()
         if 'observacoes' in data_dict:
             consulta.observacoes = data_dict['observacoes']
         if 'status' in data_dict:
@@ -62,3 +73,20 @@ class ConsultaService:
         
         return query.all()
         
+def validar_disponibilidade_consulta(data, hora, dentista_id):
+    """
+    Raises:
+        HorarioDentistaError: Se não tiver horario disponivel.
+    """
+    query = Consulta.query.with_entities(Consulta.hora).filter(Consulta.data == data, Consulta.dentista_id == dentista_id).all()
+    if query:
+        for horario in query:
+            # não pode ser no mesmo horario e n pode ter menos de 30 min de diferença de outra consulta
+            diferenca = (horario - hora)
+            if diferenca < 30 and  diferenca > -30:
+                raise HorarioDentistaError(f' horario indiponivel devido a agenda do Dentista', 400)
+
+class HorarioDentistaError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
