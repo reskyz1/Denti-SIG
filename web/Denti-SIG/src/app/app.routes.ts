1782a1ee867815f250558705ed1e5f3c650fd02e
Luis Felipe Revoredo <luismarinho501@gmail.com>
2025-06-24 20:44:33 -0300

    tela de escolha do tipo de usuário para cadastro

--- web/Denti-SIG/src/app/app.routes.ts
+++ web/Denti-SIG/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
+    {path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)}
 ];
---
+++ web/Denti-SIG/src/app/features/auth/register/register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  standalone: true,
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent {
+  
+}
