cc2f29c0028455a98af50c9d9580f00c84ed184d
Luis Felipe Revoredo <luismarinho501@gmail.com>
2025-06-24 23:17:59 -0300

    tela de cadastro de paciente

--- web/Denti-SIG/src/app/app.routes.ts
+++ web/Denti-SIG/src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
+    {path: '', redirectTo: 'login', pathMatch: 'full' },
+    {path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent) },
     {path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)},
-    {path: 'register-dentist', loadComponent: () => import('./features/auth/register-dentist/register-dentist.component').then(m => m.RegisterDentistComponent)}
+    {path: 'register-dentist', loadComponent: () => import('./features/auth/register-dentist/register-dentist.component').then(m => m.RegisterDentistComponent)},
+    {path: 'register-patient', loadComponent: () => import('./features/auth/register-patient/register-patient.component').then(m => m.RegisterPatientComponent)}
 ];
