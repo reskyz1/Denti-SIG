dd28a59b567b7b450de045970edc11e27ef6ef6f
Luis Felipe <luis.revoredo@ufrpe.br>
2025-07-19 00:28:44 -0300

    filtragem da agenda pelo dentista

--- web/Denti-SIG/src/app/pages/initial/initial.component.ts
+++ web/Denti-SIG/src/app/pages/initial/initial.component.ts
@@ -11,6 +11,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import ptLocale from '@fullcalendar/core/locales/pt-br';
 import { isPlatformBrowser, CommonModule } from '@angular/common';
 import { EventInput } from '@fullcalendar/core';
+import { Dentista } from 'src/app/models/dentista';
 
 @Component({
   selector: 'app-initial',
@@ -31,7 +32,9 @@ import { EventInput } from '@fullcalendar/core';
 export class InitialComponent {
   selectedDate: Date = new Date();
   calendarOptions: CalendarOptions | undefined;
-  eventList?: EventInput[]
+  eventList?: EventInput[];
+  dentistas: Dentista[] = [];
+  dentistaSelecionado?: Dentista 
   
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {
     if (isPlatformBrowser(this.platformId)) {
@@ -48,7 +51,55 @@ export class InitialComponent {
        height: 'auto'
       };
     }
+
+    const dentista1 : Dentista = {
+      nome: "Luis Felipe",
+      email: "luismarinho501@gmail.com",
+      cpf: "70336034440",
+      telefone: "(81)983089297",
+      senha: "12345678",
+      cro: "123",
+      id:1
+    };
+    const dentista2: Dentista = {
+      nome: "Ana Beatriz",
+      email: "ana.bia@clinic.com",
+      cpf: "12345678900",
+      telefone: "(81)981234567",
+      senha: "senhaAna123",
+      cro: "456",
+      id: 2
+    };
+
+    const dentista3: Dentista = {
+      nome: "Carlos Eduardo",
+      email: "carlos.edu@clinic.com",
+      cpf: "98765432100",
+      telefone: "(81)982345678",
+      senha: "carlos2025",
+      cro: "789",
+      id: 3
+    };
+
+    const dentista4: Dentista = {
+      nome: "Juliana Costa",
+      email: "juliana.costa@clinic.com",
+      cpf: "11223344556",
+      telefone: "(81)980123456",
+      senha: "julianaSecure!",
+      cro: "321",
+      id: 4
+    };
+
+    this.dentistas?.push(dentista1,dentista2,dentista3,dentista4)
   }
 
- 
+  selecionarDentista(dentista: Dentista){
+    this.dentistaSelecionado = dentista;
+  }
+
+  abrirPopupConsulta() {
+  console.log("Abrir popup de marcação de consulta");
+  // futuro: abrir modal de cadastro
+}
 }
